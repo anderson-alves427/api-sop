f9c747ca78687e42092b069d1416bb3f8d89d9da
anderson-alves427 <oliveiraalves427@gmail.com>
2025-03-23 15:18:06 -0300

    feat: list all despesas

--- src/main/java/br/com/andersonalves/sop_api/modules/despesa/controllers/DespesaController.java
+++ src/main/java/br/com/andersonalves/sop_api/modules/despesa/controllers/DespesaController.java
@@ -1,7 +1,10 @@
 package br.com.andersonalves.sop_api.modules.despesa.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.andersonalves.sop_api.modules.despesa.entities.DespesaEntity;
 import br.com.andersonalves.sop_api.modules.despesa.services.CreateDespesaService;
+import br.com.andersonalves.sop_api.modules.despesa.services.ListAllDespesaService;
 import jakarta.validation.Valid;
 
 @RestController
@@ -17,6 +21,9 @@ public class DespesaController {
 
     @Autowired
     private CreateDespesaService createDespesaService;
+
+    @Autowired
+    private ListAllDespesaService listAllDespesaService;
 
     @PostMapping("")
     public ResponseEntity<Object> create(@Valid @RequestBody DespesaEntity despesaEntity) {
@@ -27,4 +34,11 @@ public class DespesaController {
             return ResponseEntity.badRequest().body(e.getMessage());
         }
     }
+
+    @GetMapping("")
+    public ResponseEntity<List<DespesaEntity>> listAllDespesa() {
+        List<DespesaEntity> despesas = this.listAllDespesaService.execute();
+        return ResponseEntity.ok(despesas);
+    }
+
 }
---
+++ src/main/java/br/com/andersonalves/sop_api/modules/despesa/services/ListAllDespesaService.java
@@ -0,0 +1,20 @@
+package br.com.andersonalves.sop_api.modules.despesa.services;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import br.com.andersonalves.sop_api.modules.despesa.entities.DespesaEntity;
+import br.com.andersonalves.sop_api.modules.despesa.repositories.DespesaRepository;
+
+@Service
+public class ListAllDespesaService {
+
+    @Autowired
+    private DespesaRepository despesaRepository;
+
+    public List<DespesaEntity> execute() {
+        return this.despesaRepository.findAll();
+    }
+}
