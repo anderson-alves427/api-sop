6a125762d48f277ec7be753341b06059c0cb8fa8
anderson-alves427 <oliveiraalves427@gmail.com>
2025-03-26 17:34:37 -0300

    config condfig cors

---
+++ src/main/java/br/com/andersonalves/sop_api/config/WebConfig.java
@@ -0,0 +1,20 @@
+package br.com.andersonalves.sop_api.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    // Configuração somente para ambientes de testes dessa aplicação. Isso é tratado
+    // com muito mais cuidado
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(false);
+    }
+}
